2e4623589049c1ff39cf3e17046920b5f6dc29ad
ItsRgautam <rahulgautamras5@gmail.com>
2024-05-18 08:54:33 +0530

    final commit

--- src/main/java/ChatApplicationProject/Configuration/WebConfig.java
+++ src/main/java/ChatApplicationProject/Configuration/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","https://connectify-silk.vercel.app")
+                        .allowedOrigins("http://localhost:3000","https://connecti-fyy.vercel.app")
                         .allowedMethods(HttpMethod.GET.name(),
                                 HttpMethod.POST.name(),
                                 HttpMethod.DELETE.name(),
